3276a78983df7459adf18a9a099f9f5dcaffce3f
Jefte Sales <jefte.green@gmail.com>
2023-06-08 21:13:26 -0300

    update file

--- src/components/forms/AddFoodForm.jsx
+++ src/components/forms/AddFoodForm.jsx
@@ -3,20 +3,30 @@ import React, { useState } from 'react';
 const AddFoodForm = ({ addFood }) => {
   const [foodName, setFoodName] = useState('');
   const [foodValue, setFoodValue] = useState('');
+  const [canBeShared, setCanBeShared] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (foodName && foodValue) {
-      addFood(foodName, parseFloat(foodValue));
+      const parsedValue = parseFloat(foodValue);
+      const value = canBeShared ? parsedValue : Math.round(parsedValue);
+      addFood(foodName, value);
       setFoodName('');
       setFoodValue('');
+      setCanBeShared(false);
     }
+  };
+
+  const handleCheckboxChange = (e) => {
+    setCanBeShared(e.target.checked);
+    console.log(canBeShared)
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="foodName">Nome da Comida:</label>
       <input type="text" id="foodName" value={foodName} onChange={(e) => setFoodName(e.target.value)} required />
+      
       <label htmlFor="foodValue">Valor da Comida:</label>
       <input
         type="number"
@@ -26,9 +36,17 @@ const AddFoodForm = ({ addFood }) => {
         step="0.01"
         required
       />
+      
+      <div>
+        <label htmlFor="canBeShared">Comida pode ser dividida?</label>
+        <select id="canBeShared" value={canBeShared} onChange={(e) => setCanBeShared(e.target.value === 'true')}>
+          <option value="true">Sim</option>
+          <option value="false">Não</option>
+        </select>
+      </div>
+      
       <button type="submit">Adicionar Comida</button>
     </form>
   );
 };
-
 export default AddFoodForm;
--- src/components/lists/ResultsList.jsx
+++ src/components/lists/ResultsList.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 
-const ResultsList = ({ people, foods }) => {
+const ResultsList = ({ people, foods, canBeShared }) => {
   const calculateTotalValue = (person) => {
     let totalValue = 0;
     person.foods.forEach((foodName) => {
       const food = foods.find((f) => f.name === foodName);
       if (food) {
-        totalValue += food.value;
+        let valueToAdd = food.value;
+        if (canBeShared === true && food.value % 1 === 0) {
+          const numberOfPeopleWithFood = people.filter((person) => person.foods.includes(foodName)).length;
+          valueToAdd = food.value / numberOfPeopleWithFood;
+        }
+        totalValue += valueToAdd;
       }
     });
-  
+
     return totalValue.toFixed(2);
   };
-  
- 
 
   return (
     <ul>
